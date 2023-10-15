532f02cf796559127b8d579f98d0a7a0bc743eea
Camilo Sanchez <camilo.sanchez@heru-app.com>
2023-10-15 08:11:34 -0500

    Log private key

--- app/commons/gcp.py
+++ app/commons/gcp.py
@@ -5,6 +5,7 @@ from google.cloud import pubsub_v1
 from google.pubsub_v1 import PullRequest
 from google.oauth2.service_account import Credentials
 
+print(os.environ["GOOGLE_CLOUD_PRIVATE_KEY"])
 creds = Credentials.from_service_account_info(
     {
         "type": os.environ["GOOGLE_CLOUD_TYPE"],
