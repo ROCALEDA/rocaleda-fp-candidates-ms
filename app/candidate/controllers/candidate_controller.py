2c65626bf0b74cce80798c1a4c30d1ff47c458e2
Camilo Sanchez <camilo.sanchez@heru-app.com>
2023-10-18 08:39:13 -0500

    Fix data encoding in candidates processing

--- app/candidate/controllers/candidate_controller.py
+++ app/candidate/controllers/candidate_controller.py
@@ -1,3 +1,5 @@
+import base64
+import json
 from fastapi import APIRouter, Body, HTTPException
 from app.database.schemas import PubSubMessage, CreateCandidate
 from typing import TYPE_CHECKING
@@ -20,8 +22,11 @@ def initialize(candidate_service: "CandidateService"):
         message = data.message
         if not message:
             raise HTTPException(status_code=400, detail="Invalid message format")
-        print("Received message from pubsub: ", message)
-        candidate = CreateCandidate(**message)
+        decoded_data = base64.b64decode(message["data"]).decode("utf-8")
+        data_dict = json.loads(decoded_data)
+
+        print("Received message from pubsub: ", data_dict)
+        candidate = CreateCandidate(**data_dict)
         await candidate_service.create_candidate(candidate)
 
         return {"success": True}
--- tests/candidate/controllers/test_candidate_controller.py
+++ tests/candidate/controllers/test_candidate_controller.py
@@ -1,4 +1,6 @@
 import pytest
+import json
+import base64
 from fastapi import HTTPException
 from unittest.mock import Mock, AsyncMock
 from app.candidate.controllers import candidate_controller
@@ -10,14 +12,16 @@ class TestUserController:
         mock_service = Mock()
         mock_service.create_candidate = AsyncMock()
 
-        data_mock = Mock(
-            message={
-                "user_id": 1,
-                "fullname": "John Doe",
-                "tech_skills": [],
-                "soft_skills": [],
-            }
-        )
+        data_to_encode = {
+            "user_id": 1,
+            "fullname": "John Doe",
+            "tech_skills": [],
+            "soft_skills": [],
+        }
+        json_str = json.dumps(data_to_encode)
+        encoded_data = base64.b64encode(json_str.encode("utf-8"))
+
+        data_mock = Mock(message={"data": encoded_data})
 
         create_candidate_func = candidate_controller.initialize(mock_service)[
             "create_candidate_from_push"
