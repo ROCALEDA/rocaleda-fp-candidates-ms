8e815bc7877040de4caf942125873f505dc940c4
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2023-11-12 14:18:29 -0500

    feat: se reajusta modelo de datos

--- app/interview/repositories/interview_repository.py
+++ app/interview/repositories/interview_repository.py
@@ -6,24 +6,28 @@ class InterviewRepository:
         self, candidate_id: int, page: int, page_size: int
     ):
         with database.create_session() as db:
-            query = db.query(models.Interview).filter_by(candidate_id=candidate_id)
-
+            query = (
+                db.query(models.Interview)
+                .filter_by(candidate_id=candidate_id)
+                .order_by(models.Interview.realization_date)
+            )
             total_count = query.count()
             total_pages = (total_count + page_size - 1) // page_size
             offset = (page - 1) * page_size
             query = query.offset(offset).limit(page_size)
-
-            return {"data": query, "total_pages": total_pages}
+            return {"data": query.all(), "total_pages": total_pages}
 
     async def get_customer_interviews(
         self, customer_id: int, page: int, page_size: int
     ):
         with database.create_session() as db:
-            query = db.query(models.Interview).filter_by(customer_id=customer_id)
-
+            query = (
+                db.query(models.Interview)
+                .filter_by(customer_id=customer_id)
+                .order_by(models.Interview.realization_date)
+            )
             total_count = query.count()
             total_pages = (total_count + page_size - 1) // page_size
             offset = (page - 1) * page_size
             query = query.offset(offset).limit(page_size)
-
             return {"data": query.all(), "total_pages": total_pages}
