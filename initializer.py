b37d18b7f7a47083368efd741d58167b4df9c3f2
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2023-11-12 00:10:23 -0500

    feat: adición consulta de entrevistas asociadas a usuario logueado más pruebas unitarias

--- initializer.py
+++ initializer.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
 from app.database import models, database
-from app.health.controllers import health_controller
 from app.candidate.controllers import candidate_controller
-from app.health.services.health_service import HealthService
 from app.candidate.services.candidate_service import CandidateService
 from app.candidate.repositories.candidate_repository import CandidateRepository
+from app.health.controllers import health_controller
+from app.health.services.health_service import HealthService
+from app.interview.controllers import interview_controller
+from app.interview.services.interview_service import InterviewService
+from app.interview.repositories.interview_repository import InterviewRepository
 
 
 class Initializer:
@@ -15,8 +18,9 @@ class Initializer:
 
     def setup(self):
         self.init_health_module()
-        self.init_candidate_module()
         self.init_database()
+        self.init_candidate_module()
+        self.init_interview_module()
 
     def init_health_module(self):
         health_service = HealthService()
@@ -29,6 +33,12 @@ class Initializer:
         candidate_controller.initialize(candidate_service)
         self.app.include_router(candidate_controller.router)
         self.candidate_service = candidate_service
+
+    def init_interview_module(self):
+        interview_repository = InterviewRepository()
+        interview_service = InterviewService(interview_repository)
+        interview_controller.initialize(interview_service)
+        self.app.include_router(interview_controller.router)
 
     def init_database(self):
         models.Base.metadata.create_all(bind=database.engine)
